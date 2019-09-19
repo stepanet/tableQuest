52b96ee7c94abd6dcf86cec5c09e32867251bce1
stepanet <stepanet@gmail.com>
2019-09-19 21:15:44 +0300

    Initial Commit

---
+++ tableQuest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableQuest
+//
+//  Created by Jack Sp@rroW on 19.09.2019.
+//  Copyright © 2019 Jack Sp@rroW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
